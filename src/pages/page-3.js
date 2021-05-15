3cad2d77c6ae4dedae88c6ab7587d5518f8388bb
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-05-15 20:35:24 +0200

    Update page-3.js

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -5,23 +5,37 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-
 class Faqs extends React.Component {
    constructor(props) {
    super(props);
    this.state = {
-     expand: true
+     color: true
    };
   }
   toggle = () => {
-    this.setState({ expand: !this.state.expand });
+    this.setState({ color: !this.state.color });
   }
   render = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Testimg change button</h1>
-     <p className={this.state.expand ? 'Red' : 'Blue'}>This is my {this.state.expand ? 'Red' : 'Blue'} book.</p>
-        <button type="button" onClick={this.toggle.bind(this)}>Change Color</button>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+     <p className={this.state.color ? 'Red' : 'Blue'}>This is my {this.state.color ? 'Red' : 'Blue'} book.</p>
+        <button type="button" onClick={this.toggle.bind(this)}>Change color</button>
+
   </Layout>
 )
 }
