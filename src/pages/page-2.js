a9370d0a205d939dfa0e3d093eee34c7ec642a64
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-05-15 16:05:14 +0200

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,11 +4,36 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+
+class ToggleButton extends React.Component {
+   constructor(props) {
+   super(props);
+   this.state = {
+     color: true
+   }
+  }
+  
+  toggle = () => {
+    this.setState({ color: !this.state.color });
+  }
+  render = props => (
+  <button type="button" 
+    className={this.state.color ? 'Mine Red' : 'Mine Blue'}
+    onClick={this.toggle.bind(this)}>
+    Change color 
+  </button>
+  )
+}
+
+
+const SecondPage = (props) => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <p>The color is {this.state.color ? 'Red' : 'Blue'} </p>
+
+    <ToggleButton />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
