5eef1308df29a573e13437d7259f02a2953b9366
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-06-24 20:41:41 +0200

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,11 +10,11 @@ import React from 'react'
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-	<script type="text/javascript">
+	<script>
 	dangerouslySetInnerHTML={{__html:" piAId = 'a'; piCId = 'a'; piHostname = 'pi.pardot.com'; (function() { function async_load(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js'; var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);} if(window.attachEvent) { window.attachEvent('onload', async_load); } else { window.addEventListener('load', async_load, false); }})(); "
 	}}
 	</script>,
-	<script type="text/javascript">
+	<script>
 		dangerouslySetInnerHTML={{__html:" piAId = 'b';  piCId = 'b';  piHostname = 'pi.pardot.com';  (function() { function async_load(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js'; var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);} if(window.attachEvent) { window.attachEvent('onload', async_load); } else { window.addEventListener('load', async_load, false); }})(); "
 	}}
 	</script>
